9e84f6e2ae5b750ba97510e472a1cd2cfc031963
CanIHasCookie125 <41319199+CanIHasCookie125@users.noreply.github.com>
2018-09-14 07:45:31 -0700

    Easy Challenge #20.cpp
    https://www.reddit.com/r/dailyprogrammer/comments/qnkro/382012_challenge_20_easy/

---
+++ Easy Challenge #20.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <iostream>
+#include <vector>           // My first time using std::vector
+
+int main(int argc, char *argv[])
+{
+    const int n = 2000;
+    
+    std::vector<bool> primeList ( n, true);      //Assume all prime (true)!
+    
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (primeList[i] == true)                   //Ignore confirmed prime.
+        {
+            for (int p = i *2; p <= n; p += i)      //All multiples are composite.
+                primeList[p] = false;
+
+        }
+        
+    }
+    
+for(int i=2;  i<=n ; i++)
+    if (primeList[i] == true) {
+        std::cout <<i << " ";
+    }
+
+    
+    return 0;
+}
