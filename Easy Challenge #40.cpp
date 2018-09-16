64a38b2b8bd8fed77a33ef64fc9a8bed0737a63b
CanIHasCookie125 <41319199+CanIHasCookie125@users.noreply.github.com>
2018-09-16 00:53:57 -0700

    Easy Challenge #40
    https://www.reddit.com/r/dailyprogrammer/comments/schtf/4162012_challenge_40_easy/

---
+++ Easy Challenge #40.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+int x = 0;
+
+void X() {
+    ++x;
+    std::cout << x << " ";
+}
+
+void Y() {
+    X();
+    X();
+    X();
+    X();
+    X();
+
+}
+
+void Z() {
+    Y();
+    Y();
+    Y();
+    Y();
+    Y();
+}
+
+void P() {
+    Z();
+    Z();
+    Z();
+    Z();
+    Z();
+}
+
+int main() {
+    P();
+    P();
+    P();
+    P();
+    P();
+    P();
+    P();
+    P();
+
+
+    return 0;
+}
